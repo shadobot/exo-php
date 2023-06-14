9358f23cf3b1aa6cfee1ce6e5c1e4ae21415f5db
shadobot <keitamohamed10@gmail.com>
2023-06-14 23:38:15 +0200

    first commit

---
+++ programme/1-facile/exo1/index.php
@@ -0,0 +1,16 @@
+<?php ob_start(); //NE PAS MODIFIER 
+$titre = "Exo 1 : Variables"; //Mettre le nom du titre de la page que vous voulez
+?>
+
+<!-- mettre ici le code -->
+
+
+
+<?php
+/************************
+ * NE PAS MODIFIER
+ * PERMET d INCLURE LE MENU ET LE TEMPLATE
+ ************************/
+    $content = ob_get_clean();
+    require "../../global/common/template.php";
+?>
